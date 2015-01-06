561e444cd0037c1a9d8cc8b914ef8dcf49a815bb
tonylampada <tonylampada@gmail.com>
2015-01-06 18:59:03 -0200

    update

--- bkp.sh
+++ bkp.sh
@@ -1,5 +1,5 @@
 cd $JENKINS_HOME
-rsync -a config.xml hudson.* jenkins.* jobs/ users/ *.xml $REPO_BKP
+rsync -av config.xml hudson.* jenkins.* jobs/ users/ *.xml $REPO_BKP
 git add .
 git commit -m 'backup'
 git push
